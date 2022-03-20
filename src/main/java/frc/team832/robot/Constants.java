aaf962d41725092f6b04320ba60c577abeee9c7d
Dev Sharma <d3v.shar@gmail.com>
2022-03-20 13:22:27 -0400

    add separate kp to each flywheel based on sysid data

--- src/main/java/frc/team832/robot/Constants.java
+++ src/main/java/frc/team832/robot/Constants.java
@@ -112,11 +112,9 @@ public final class Constants {
 		
 		/** Mechanical Characteristics **/ // Note: both flywheels are mechanically identical.
 		private static final double COLSON_4x2IN_MOI_KG_M2 = 0.929 * LB_IN2_TO_KG_M2;
-		public static final double KP = 0.05;
 		public static final double SHOOTER_REDUCTION = 1; 
 		public static final WheeledPowerTrain POWER_TRAIN = new WheeledPowerTrain(new Gearbox(SHOOTER_REDUCTION), Motor.kFalcon500, 1, 4, 1);
 		public static final double MOI_KGM2 = COLSON_4x2IN_MOI_KG_M2 * 2;
-		// public static final SimpleMotorFeedforward FEEDFORWARD = new SimpleMotorFeedforward(0, 1 / Motor.kFalcon500.KvRPMPerVolt);
 
 		/** System Control Values **/ // Data from sysid
 		public static final double BOTTOM_KS = 0.52828;
