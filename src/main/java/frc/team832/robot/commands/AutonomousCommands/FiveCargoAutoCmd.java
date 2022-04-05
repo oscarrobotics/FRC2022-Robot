75b5da5a3bd4a72ca30e8fd881ee97ab0d6283d8
Banks Troutman <btrout.dhrs@gmail.com>
2022-04-05 03:38:39 -0400

    Add 5ball auto

---
+++ src/main/java/frc/team832/robot/commands/AutonomousCommands/FiveCargoAutoCmd.java
@@ -0,0 +1,51 @@
+package frc.team832.robot.commands.AutonomousCommands;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.team832.robot.commands.AcceptBallCommand;
+import frc.team832.robot.commands.ShootBallVisionCmd;
+import frc.team832.robot.subsystems.ConveyorSubsystem;
+import frc.team832.robot.subsystems.DrivetrainSubsystem;
+import frc.team832.robot.subsystems.IntakeSubsystem;
+import frc.team832.robot.subsystems.ShooterSubsystem;
+
+public class FiveCargoAutoCmd extends SequentialCommandGroup {
+	public final Trajectory initialPath;
+	public final Trajectory secondPath;
+
+	/**
+	 * Five cargo autonomous.
+	 * 
+	 * Steps: 
+	 * 	1) Shoot preload
+	 * 	2) ParallelRace intake with first path
+	 * 	3) Shoot two
+	 * 	4) ParallelRace intake with second path
+	 * 	5) Shoot two
+	 */
+	public FiveCargoAutoCmd(
+		DrivetrainSubsystem drivetrain,
+		IntakeSubsystem intake,
+		ConveyorSubsystem conveyor,
+		ShooterSubsystem shooter
+	) {
+		initialPath = drivetrain.initializePaths("5 Ball Auto 01", 4, 6);
+		secondPath = drivetrain.initializePaths("5B Ball Auto 02", 4, 6);
+		addRequirements(drivetrain, intake, conveyor, shooter);
+		addCommands(
+			new ShootBallVisionCmd(conveyor, shooter),
+			new ParallelRaceGroup(
+				new AcceptBallCommand(intake, shooter, conveyor),
+				drivetrain.getTrajectoryCommand(initialPath)
+			),
+			new ShootBallVisionCmd(conveyor, shooter),
+			new ParallelRaceGroup(
+				new AcceptBallCommand(intake, shooter, conveyor),
+				drivetrain.getTrajectoryCommand(secondPath)
+			),
+			new ShootBallVisionCmd(conveyor, shooter)
+		);
+	}
+	
+}
